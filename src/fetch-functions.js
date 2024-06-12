de6f2df747f7adc2f810bf6fe9ce9bd4b8949779
VeshonaS <aniahshell1@gmail.com>
2024-06-12 16:07:42 -0400

    1 done

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,9 +1,26 @@
 const userUrl = 'https://jsonplaceholder.typicode.com/users'
 
 export const checkResponseStatus = () => {
+  return fetch('https://jsonplaceholder.typicode.com/users')
+     .then(res => {
+      const object = {
+        "ok":res.ok,
+        "status":res.status,
+        "url":res.url
+        }
+        return object
+     });
+    //   if (!res.ok){
+    //     console.error('check the url')
+    //   } 
+    // // })
+    //   .then(data => data)
+    //   .catch(error => `Fetch error: ${error.message}`)
 };
 
+
 export const getUsers = () => {
+  return fetch(userUrl)
 };
 
 export const getUserPosts = () => {
