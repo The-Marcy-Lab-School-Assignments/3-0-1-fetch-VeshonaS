26f6231c1b8ce3e2b0822e6e7829c99d9a529280
Itzel01 <itzel.areymi.rendon@gmail.com>
2024-06-23 20:28:12 -0400

    feedback

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import {
   createNewUser,
   getUsers
 } from './fetch-functions.js';
-
+/** FEEDBACK: Great job getting most test cases to pass! */
 export default function app(appDiv) {
   // fetch('https://jsonplaceholder.typicode.com/users')
   const { statusDiv, usersUl, postsUl, newUserForm, newUserDiv } = setupPageBasics(appDiv)
@@ -19,10 +19,13 @@ export default function app(appDiv) {
   getUsers().then(users => renderUsers(usersUl,users ));
   usersUl.addEventListener('click', (e) => {
     if (e.target.matches('button')){ 
+      /** FEEDBACK: You were so close! You should save the user's id in a variable! Like so: 
+       * const userID = e.target.dataset.userId
+       */
       e.target.dataset.userId = userID
       getUserPosts(userID).then(post => renderPosts(postsUl, post))
     }})
-  
+  /** FEEDBACK: You are on the right track! You got this! */
 //  getUserPosts().then(status => renderPosts(postsUl, status))
 //  createNewUser().then(status => renderNewUser(newUserDiv, status ))
 }
